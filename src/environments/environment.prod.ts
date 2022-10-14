c67b86e7a7c2f76c7bec1850669b5857ee955505
DESKTOP-CU7PFRA\VNPT <havantruong@vnpt.vn>
2022-10-14 09:58:29 +0700

    add production api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true, 
-  backEndApiURL: 'http://localhost:3000/admin-api/',
+  //backEndApiURL: 'http://localhost:3000/admin-api/',
+  backEndApiURL: 'https://appstore-vnpt-api.herokuapp.com/',
 };
